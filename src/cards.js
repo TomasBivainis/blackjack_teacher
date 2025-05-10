fc486dba5ee085ef9e1cd09b5d378c60d99ab8c4
Tomas Bivainis <62440370+TomasBivainis@users.noreply.github.com>
2025-05-10 23:00:49 +0300

    Revert "Refactor"

---
+++ src/cards.js
@@ -0,0 +1,118 @@
+import back from "./images/cards/back.png";
+
+import C10 from "./images/cards/0C.png";
+import C2 from "./images/cards/2C.png";
+import C3 from "./images/cards/3C.png";
+import C4 from "./images/cards/4C.png";
+import C5 from "./images/cards/5C.png";
+import C6 from "./images/cards/6C.png";
+import C7 from "./images/cards/7C.png";
+import C8 from "./images/cards/8C.png";
+import C9 from "./images/cards/9C.png";
+import CJ from "./images/cards/JC.png";
+import CQ from "./images/cards/QC.png";
+import CK from "./images/cards/KC.png";
+import CA from "./images/cards/AC.png";
+
+import H10 from "./images/cards/0H.png";
+import H2 from "./images/cards/2H.png";
+import H3 from "./images/cards/3H.png";
+import H4 from "./images/cards/4H.png";
+import H5 from "./images/cards/5H.png";
+import H6 from "./images/cards/6H.png";
+import H7 from "./images/cards/7H.png";
+import H8 from "./images/cards/8H.png";
+import H9 from "./images/cards/9H.png";
+import HJ from "./images/cards/JH.png";
+import HQ from "./images/cards/QH.png";
+import HK from "./images/cards/KH.png";
+import HA from "./images/cards/AH.png";
+
+import D10 from "./images/cards/0D.png";
+import D2 from "./images/cards/2D.png";
+import D3 from "./images/cards/3D.png";
+import D4 from "./images/cards/4D.png";
+import D5 from "./images/cards/5D.png";
+import D6 from "./images/cards/6D.png";
+import D7 from "./images/cards/7D.png";
+import D8 from "./images/cards/8D.png";
+import D9 from "./images/cards/9D.png";
+import DJ from "./images/cards/JD.png";
+import DQ from "./images/cards/QD.png";
+import DK from "./images/cards/KD.png";
+import DA from "./images/cards/AD.png";
+
+import S10 from "./images/cards/0S.png";
+import S2 from "./images/cards/2S.png";
+import S3 from "./images/cards/3S.png";
+import S4 from "./images/cards/4S.png";
+import S5 from "./images/cards/5S.png";
+import S6 from "./images/cards/6S.png";
+import S7 from "./images/cards/7S.png";
+import S8 from "./images/cards/8S.png";
+import S9 from "./images/cards/9S.png";
+import SJ from "./images/cards/JS.png";
+import SQ from "./images/cards/QS.png";
+import SK from "./images/cards/KS.png";
+import SA from "./images/cards/AS.png";
+
+const cards = {
+  back: back,
+  C10: C10,
+  C2: C2,
+  C3: C3,
+  C4: C4,
+  C5: C5,
+  C6: C6,
+  C7: C7,
+  C8: C8,
+  C9: C9,
+  CJ: CJ,
+  CQ: CQ,
+  CK: CK,
+  CA: CA,
+
+  H10: H10,
+  H2: H2,
+  H3: H3,
+  H4: H4,
+  H5: H5,
+  H6: H6,
+  H7: H7,
+  H8: H8,
+  H9: H9,
+  HJ: HJ,
+  HQ: HQ,
+  HK: HK,
+  HA: HA,
+
+  D10: D10,
+  D2: D2,
+  D3: D3,
+  D4: D4,
+  D5: D5,
+  D6: D6,
+  D7: D7,
+  D8: D8,
+  D9: D9,
+  DJ: DJ,
+  DQ: DQ,
+  DK: DK,
+  DA: DA,
+
+  S10: S10,
+  S2: S2,
+  S3: S3,
+  S4: S4,
+  S5: S5,
+  S6: S6,
+  S7: S7,
+  S8: S8,
+  S9: S9,
+  SJ: SJ,
+  SQ: SQ,
+  SK: SK,
+  SA: SA,
+};
+
+export default cards;
